b2e555c0e61c2cc535b84ee73297e45536ebabe7
Recep Terzi <recepterzi-67@hotmail.com>
2022-08-25 19:50:14 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { useSelector } from "react-redux";
-import Contacts from "./components/Contacts/Contacts";
-import List from "./components/List/List";
-import { contactSelectors } from "./redux/contactSlice";
 import { Routes, Route } from "react-router-dom";
 import AllComponents from "./components/AllComponents";
 import Edit from "./components/Edit/Edit";
+import Error from "./components/Error/Error";
 function App() {
   return (
     <div className="App">
@@ -12,6 +9,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<AllComponents />} />
           <Route exact path="/edit/:id" element={<Edit />} />
+          <Route exact path="*" element={<Error />} />
         </Routes>
       </div>
     </div>
